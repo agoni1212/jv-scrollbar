e45ff77af15bdb8e0de5e6721a1c4ea9904e6e61
agoni1212 <agoni1212@gmail.com>
2019-01-05 22:50:59 +0800

    update babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
+  mode: "production",
   entry: './src/components/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -11,6 +12,9 @@ module.exports = {
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
+  optimization: {
+    minimize: true
+  },
   module: {
     rules: [
       {
@@ -19,26 +23,11 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ],
-      },      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
       }
     ]
   },
@@ -56,26 +45,5 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+  devtool: 'source-map'
 }
